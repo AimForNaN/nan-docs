6e73ca18396b2181d76c99df444bdbd9280c3999
AimForNaN <aimfornan@localhost>
2026-04-05 13:23:47 -0400

    Github pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aimfornan.github.io',
+	base: '/nan-docs',
 	integrations: [
 		starlight({
 			title: 'NaN',
